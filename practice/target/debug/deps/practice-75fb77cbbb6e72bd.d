1d167fcef915328c2b7cdd33cc88e78fbc1e980d
omelet1 <omelet3032@gmail.com>
2026-01-03 06:04:17 +0900

    반복자 연습

---
+++ practice/target/debug/deps/practice-75fb77cbbb6e72bd.d
@@ -0,0 +1,5 @@
+/home/omelet/dev/rust-with-gemini/practice/target/debug/deps/practice-75fb77cbbb6e72bd.d: src/main.rs
+
+/home/omelet/dev/rust-with-gemini/practice/target/debug/deps/libpractice-75fb77cbbb6e72bd.rmeta: src/main.rs
+
+src/main.rs:
---
+++ practice/target/debug/deps/practice-e304dc8bfb547617.d
@@ -0,0 +1,5 @@
+/home/omelet/dev/rust-with-gemini/practice/target/debug/deps/practice-e304dc8bfb547617.d: src/main.rs
+
+/home/omelet/dev/rust-with-gemini/practice/target/debug/deps/libpractice-e304dc8bfb547617.rmeta: src/main.rs
+
+src/main.rs:
